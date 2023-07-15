140da3dfedc98491267f888dd969ec6426b829b1
nissy <c0a21099e4@edu.teu.ac.jp>
2023-07-15 09:18:08 +0900

    [edit] NewStep1.jsx, NewStep2.jsxの作成&MaterialUIの採用

--- virtual_namecard/src/App.js
+++ virtual_namecard/src/App.js
@@ -3,7 +3,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Step1 from './Step1';
+import NewStep1 from './NewStep1';
 import Step2 from './Step2';
+import NewStep2 from './NewStep2';
 import Step3 from './Step3';
 import Result from './Result';
 import Home from './Home'; // Homeコンポーネントのインポートを追加
@@ -21,7 +23,9 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/newhome" element={<NewHome />} />
           <Route path="/step1" element={<Step1 />} />
+          <Route path="/newstep1" element={<NewStep1 />} />
           <Route path="/step2" element={<Step2 />} />
+          <Route path="/newstep2" element={<NewStep2 />} />
           <Route path="/step3" element={<Step3 />} />
           <Route path="/result" element={<Result />} />
         </Routes>
---
+++ virtual_namecard/src/NewStep1.jsx
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Button, TextField, Typography } from '@mui/material';
+
+const Step1 = () => {
+  const [schoolCompany, setSchoolCompany] = useState('');
+  const [name, setName] = useState('');
+  const navigate = useNavigate();
+
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validateInput = () => {
+    if (schoolCompany.trim() === '' && name.trim() === '') {
+      setErrorMessage('Error!!! 学校/会社名とお名前を入力してください');
+      return false;
+    } else if (schoolCompany.trim() === '') {
+      setErrorMessage('Error!!! 学校/会社名を入力してください');
+      return false;
+    } else if (name.trim() === '') {
+      setErrorMessage('Error!!! お名前を入力してください');
+      return false;
+    }
+
+    return true;
+  };
+
+  const handleNext = () => {
+    if (validateInput()) {
+      navigate('/step2', {
+        state: {
+          schoolCompany,
+          name
+        }
+      });
+    }
+  };
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  return (
+    <div>
+      <Typography variant="h2" component="h2">
+        Step 1
+      </Typography>
+      {errorMessage && <Typography color="error">{errorMessage}</Typography>}
+      <TextField
+        label="学校名/会社名"
+        value={schoolCompany}
+        onChange={(e) => setSchoolCompany(e.target.value)}
+        // placeholder="学校名/会社名"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <TextField
+        label="お名前"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        // placeholder="お名前"
+        variant="outlined"
+        margin="normal"
+        fullWidth
+      />
+      <Button variant="contained" color="primary" onClick={handleBack}>
+        戻る
+      </Button>
+      <Button variant="contained" color="primary" onClick={handleNext}>
+        次へ
+      </Button>
+    </div>
+  );
+};
+
+export default Step1;
