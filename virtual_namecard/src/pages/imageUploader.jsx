f1acf515aad5bc78bcabe5a315b18e4b8d93f837
nissy <c0a21099e4@edu.teu.ac.jp>
2023-07-15 17:37:03 +0900

    [add] firebaseに画像をアップロードするもの(firebase.jsなどのapikeyが含まれているものはpushの除外)

--- virtual_namecard/src/pages/imageUploader.jsx
+++ virtual_namecard/src/pages/imageUploader.jsx
@@ -1,28 +1,108 @@
 import { Button } from "@mui/material";
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import { useNavigate } from 'react-router-dom';
+
 // import ImageLogo from "./image.svg";
 import "./ImageUpload.css";
 
+import storage from "../firebase";
+import {ref, uploadBytesResumable} from "firebase/storage";
+
 const ImageUploader = () => {
-  return (
-    <div className="outerBox">
-      <div className="title">
-        <h2>画像アップローダー</h2>
-        <p>JpegかPngの画像ファイル</p>
-      </div>
-      <div className="imageUplodeBox">
-        <div className="imageLogoAndText">
-          {/* <img src={ImageLogo} alt="imagelogo" /> */}
-          <p>ここにドラッグ＆ドロップしてね</p>
-        </div>
-        <input className="imageUploadInput" multiple name="imageURL" type="file"/>
-      </div>
-      <p>または</p>
-      <Button variant="contained">
-        ファイルを選択
-        <input className="imageUploadInput" type="file"/>
-      </Button>
-    </div>
+    // ローディングの状態を見ている
+    
+    const [loading, setLoading] = useState(false);
+    const [isUploaded, setUploaded] = useState(false);
+
+    const navigate = useNavigate();
+
+    const OnFileUploadToFirebase =(e) => {
+        // console.log(e.target.files[0].name);
+        const file = e.target.files[0];
+        const storageRef = ref(storage, "image/" + File.name);
+        // uploadBytes(storageRef, file).then((snapshot) => {
+        //     console.log('Uploaded a blob or file!');
+        //   });
+        // upload時間の監視
+        const uploadImage = uploadBytesResumable(storageRef, file);
+
+        uploadImage.on(
+            "state_changed",
+            // ロードが開始されたら
+            (snapshot) => {
+                setLoading(true);
+            },
+            (err) => {
+                console.log(err);
+            },
+            () => {
+                setLoading(false);
+                setUploaded(true);
+            }
+        )
+    };
+
+    // upload後の処理
+
+    useEffect(() => {
+        if (isUploaded) {
+          const timer = setTimeout(() => {
+            navigate("/newstep1"); // step1.jsxへのパスを指定してください
+          }, 5000);
+    
+          return () => clearTimeout(timer);
+        }
+      }, [isUploaded, navigate]);
+
+    return (
+        // 状態確認
+        <>{loading ? (
+            <h2> アップロード中です.</h2>
+        ) : (
+            <>
+            {isUploaded ? (
+                <h2>アップロード完了!!!...5秒後に次の画面に遷移します.</h2>
+            ) : (
+                <div className="outerBox">
+                    <div className="title">
+                        <h2>画像アップローダー</h2>
+                        <p>JpegかPngの画像ファイル</p>
+                    </div>
+                    <div className="imageUplodeBox">
+                        <div className="imageLogoAndText">
+                        {/* <img src={ImageLogo} alt="imagelogo" /> */}
+                        <p>ここにドラッグ＆ドロップしてね</p>
+                        </div>
+                        <input 
+                            className="imageUploadInput"
+                            multiple name="imageURL"
+                            type="file"
+                            accept=".png, .jpeg, .jpg"
+                            onChange={OnFileUploadToFirebase}
+                        />
+                    </div>
+                    <p>または</p>
+                    <Button variant="contained">
+                        ファイルを選択
+                        <input 
+                            className="imageUploadInput"
+                            type="file"
+                            accept=".png, .jpeg, .jpg"
+                            onChange={OnFileUploadToFirebase}
+                        />
+                    </Button>
+                </div>
+            )}
+            </>
+        )}
+
+        </>
+        
+        
+        
+    
+   
   );
 };
 
