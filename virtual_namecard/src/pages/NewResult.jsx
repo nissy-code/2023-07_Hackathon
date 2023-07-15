de8d83120df0f5445e096430338e6e304c8d5bc8
nissy <c0a21099e4@edu.teu.ac.jp>
2023-07-15 22:59:37 +0900

    [edit] NewResult.jsx (表示崩れは無くしたが, 保存時に背景の画像がでない)

--- virtual_namecard/src/pages/NewResult.jsx
+++ virtual_namecard/src/pages/NewResult.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { useLocation } from 'react-router-dom';
-import { Typography, Link } from '@mui/material';
+import { Typography, Link, Button } from '@mui/material';
 import { Twitter, GitHub } from '@mui/icons-material';
 import back from "../data/meishi1.jpg";
+import html2canvas from 'html2canvas';
 
 const Result = () => {
   const location = useLocation();
@@ -10,62 +11,131 @@ const Result = () => {
   const { schoolCompany, name, website, twitter, github, message } = state || {};
 
   const [fontSize, setFontSize] = useState('');
+  const [letterSpacing, setLetterSpacing] = useState('');
+  const cardRef = useRef(null);
 
   useEffect(() => {
     const handleResize = () => {
       const screenWidth = window.innerWidth;
-      const percentage = screenWidth <= 600 ? 1 : 2; // ウィンドウの幅に対する割合
+      const percentage = screenWidth <= 600 ? 1 : 2;
       const calculatedFontSize = `${(screenWidth * percentage) / 100}px`;
       setFontSize(calculatedFontSize);
+
+      const calculatedLetterSpacing = screenWidth <= 600 ? '-0.5px' : '0';
+      setLetterSpacing(calculatedLetterSpacing);
     };
 
-    handleResize(); // 初回レンダリング時にフォントサイズを計算
-    window.addEventListener('resize', handleResize); // リサイズイベントのリスナーを追加
+    handleResize();
+    window.addEventListener('resize', handleResize);
 
     return () => {
-      window.removeEventListener('resize', handleResize); // コンポーネントがアンマウントされる際にリスナーを解除
+      window.removeEventListener('resize', handleResize);
     };
   }, []);
 
+  const handleDownload = () => {
+    const element = cardRef.current;
+
+    html2canvas(element)
+      .then((canvas) => {
+        const link = document.createElement('a');
+        link.href = canvas.toDataURL('image/png');
+        link.download = 'output.png';
+        link.click();
+      })
+      .catch((error) => {
+        console.error('画像のダウンロードエラー:', error);
+      });
+  };
+
+  const styles = {
+    container: {
+      width: '50%',
+      margin: 'auto',
+    },
+    card: {
+      position: 'relative',
+      width: '100%',
+      paddingBottom: '40%',
+    },
+    content: {
+      position: 'absolute',
+      top: '10%',
+      left: '5%',
+      right: '5%',
+    },
+    header: {
+      marginBottom: '0.5rem',
+      fontSize,
+      letterSpacing,
+    },
+    linkContainer: {
+      display: 'flex',
+      justifyContent: 'left',
+      marginBottom: '0.5rem',
+    },
+    body: {
+      marginRight: '1rem',
+      fontSize,
+      letterSpacing,
+    },
+    iconLink: {
+      display: 'flex',
+      justifyContent: 'left',
+      alignItems: 'center',
+      marginBottom: '0.5rem',
+    },
+    message: {
+      marginBottom: '0.5rem',
+      fontSize,
+      letterSpacing,
+    },
+  };
+
   return (
-    <div>
-      <img src={back} alt="Background" style={{ width: '50%', top: '10%', paddingLeft: '25%' }} />
-      <div style={{ position: 'absolute', top: '100px', left: '25%', textAlign: 'left', paddingLeft: '1%', paddingTop: '1%' }}>
-        <Typography variant="h4" component="h4" style={{ marginBottom: '1rem', fontSize }}>
-          学校名/会社名: {schoolCompany}
-        </Typography>
-        <Typography variant="h4" component="h4" style={{ marginBottom: '1rem', fontSize }}>
-          お名前　　　 : {name}
-        </Typography>
-        <div style={{ display: 'flex', justifyContent: 'left', marginBottom: '1rem' }}> {/* justifyContentで位置変更 */}
-          <Typography variant="body1" style={{ marginRight: '1rem', fontSize }}>
-            Website:{website}
+    <div style={styles.container}>
+      <div style={styles.card}>
+        <img src={back} alt="Background" style={{ width: '100%', display: 'block' }} />
+        <div style={styles.content} ref={cardRef}>
+          <Typography variant="h4" component="h4" style={styles.header}>
+            学校名/会社名: {schoolCompany}
+          </Typography>
+          <Typography variant="h4" component="h4" style={styles.header}>
+            お名前　　　 : {name}
+          </Typography>
+          <div style={styles.linkContainer}>
+            <Typography variant="body1" style={styles.body}>
+              Website: {website}
+            </Typography>
+          </div>
+          <div style={styles.iconLink}>
+            <Typography variant="body1" style={styles.body}>
+              Twitter:
+              {twitter && (
+                <Link href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer" color="primary">
+                  <Twitter />
+                </Link>
+              )}
+            </Typography>
+          </div>
+          <div style={styles.iconLink}>
+            <Typography variant="body1" style={styles.body}>
+              GitHub:
+              {github && (
+                <Link href={`https://github.com/${github}`} target="_blank" rel="noopener noreferrer" color="primary">
+                  <GitHub />
+                </Link>
+              )}
+            </Typography>
+          </div>
+          <Typography variant="body1" style={styles.message}>
+            一言: {message}
           </Typography>
         </div>
-        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', marginBottom: '1rem' }}>
-          <Typography variant="body1" style={{ marginRight: '1rem', fontSize }}>
-            Twitter:
-            {twitter && (
-              <Link href={`https://twitter.com/${twitter}`} target="_blank" rel="noopener noreferrer" color="primary">
-                <Twitter />
-              </Link>
-            )}
-          </Typography>
-        </div>
-        <div style={{ display: 'flex', justifyContent: 'left', alignItems: 'center', marginBottom: '1rem' }}>
-          <Typography variant="body1" style={{ marginRight: '1rem', fontSize }}>
-            GitHub:
-          {github && (
-            <Link href={`https://github.com/${github}`} target="_blank" rel="noopener noreferrer" color="primary">
-              <GitHub />
-            </Link>
-          )}
-          </Typography>
-        </div>
-        <Typography variant="body1" style={{ marginBottom: '1rem', fontSize }}>
-          一言: {message}
-        </Typography>
       </div>
+      <Button variant="contained" color="primary" onClick={handleDownload} style={{ marginTop: '1rem' }}>
+        画像を保存
+      </Button>
     </div>
   );
 };
